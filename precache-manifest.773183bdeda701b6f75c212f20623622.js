6931708ec8cc542ee26325672cd9bd1487044d85
NastyaVovk <winc4ester@gmail.com>
2021-08-25 19:24:34 +0300

    Updates

---
+++ precache-manifest.773183bdeda701b6f75c212f20623622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374d195249d19e393caff47d69261fe3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c4036e1e5559c11639d",
+    "url": "./static/css/main.480643a3.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c4036e1e5559c11639d",
+    "url": "./static/js/main.afd376fb.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
